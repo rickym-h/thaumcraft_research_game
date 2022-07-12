b8ebee5b74e84fd6a2a888893eed10f41ce27562
rickym-h <richardhull11@gmail.com>
2022-07-12 13:11:00 +0100

    Initial commit

---
+++ aspect_graph.js
@@ -0,0 +1,72 @@
+console.log("aspect_graph.js initialising...")
+
+let base_aspects = [
+    "air",
+    "earth",
+    "fire",
+    "water",
+    "order",
+    "entropy"
+];
+let compound_aspects = {
+    "void": ["air", "entropy"],
+    "light": ["air", "fire"],
+    "energy": ["order", "fire"],
+    "motion": ["air", "water"],
+    "stone": ["earth", "earth"],
+    "life": ["water", "earth"],
+    "weather": ["air", "cold"],
+    "cold": ["water", "order"],
+    "crystal": ["stone", "water"],
+    "death": ["life", "entropy"],
+    "flight": ["air", "motion"],
+    "darkness": ["void", "light"],
+    "soul": ["life", "death"],
+    "heal": ["life", "life"],
+    "travel": ["motion", "earth"],
+    "poison": ["water", "death"],
+    "eldritch": ["void", "darkness"],
+    "magic": ["void", "energy"],
+    "aura": ["magic", "air"],
+    "taint": ["magic", "entropy"],
+    "seed": ["life", "order"],
+    "slime": ["life", "water"],
+    "plant": ["seed", "earth"],
+    "tree": ["air", "plant"],
+    "beast": ["motion", "life"],
+    "flesh": ["death", "beast"],
+    "undead": ["motion", "death"],
+    "mind": ["earth", "soul"],
+    "senses": ["air", "soul"],
+    "man": ["beast", "mind"],
+    "crop": ["plant", "man"],
+    "harvest": ["crop", "tool"],
+    "metal": ["stone", "order"],
+    "mine": ["man", "stone"],
+    "tool": ["man", "order"],
+    "weapon": ["tool", "entropy"],
+    "armor": ["tool", "earth"],
+    "hunger": ["life", "void"],
+    "greed": ["man", "hunger"],
+    "craft": ["man", "tool"],
+    "cloth": ["tool", "beast"],
+    "mechanism": ["motion", "tool"],
+    "trap": ["motion", "entropy"],
+    "exchange": ["motion", "water"]
+};
+
+aspect_graph = new Graph();
+for (let aspect of base_aspects) {
+    aspect_graph.add_node(aspect);
+}
+for (const [key] of Object.entries(compound_aspects)) {
+    aspect_graph.add_node(key);
+}
+for (const [key, value] of Object.entries(compound_aspects)) {
+    for (let aspect of value) {
+        aspect_graph.add_edge(key, aspect);
+        aspect_graph.add_edge(aspect, key);
+    }
+}
+
+console.log("Created aspect graph.");
---
+++ graph.js
@@ -0,0 +1,41 @@
+console.log("graph.js initialising...")
+
+class Graph {
+    constructor() {
+        this.nodes = [];
+        this.edges = {};
+        this.num_of_nodes = 0;
+        this.num_of_edges = 0;
+    }
+
+    add_node(node) {
+        this.nodes.push(node);
+        this.edges[node] = [];
+        this.num_of_nodes++;
+    }
+
+    add_edge(start, end) {
+        this.edges[start].push(end);
+        this.num_of_edges++;
+    }
+
+    get_adjacent_nodes(node) {
+        return this.edges(node);
+    }
+
+    printGraph()
+    {
+        console.log("printing graph:")
+        for (let node of this.nodes) {
+            let start = node;
+            if (this.edges[node] === []) {
+                continue;
+            }
+            let target = "";
+            for (let targetNode of this.edges[node]) {
+                target += targetNode + " ";
+            }
+            console.log(start + " -> " + target);
+        }
+    }
+}
